1bf7af29f238e53faa1464e6d730ae6e1a13ae4e
kireev_6666666@outlook.com <kireev_6666666@outlook.com>
2019-04-18 17:55:54 +0300

    ex4 - b4 - p22_25

--- B-4-Control-Operators/Practice.cs
+++ B-4-Control-Operators/Practice.cs
@@ -317,7 +317,43 @@ namespace Basic.Lesson_4._1
         //B4-P22_25 *While_Akinator100Numbers
         public static void B4_P22_25_While_Akinator100Numbers()
         {
-            
+            Console.WriteLine("Здраствуйте вас приветствует акинатор! Загадайте число от 1 до 100");
+            Console.WriteLine("Ваше число меньше 50? Отвечайте пожалуйста только Да или Нет");
+            string y = "Да";
+            string n = "Нет";
+            int a = 50;
+            int i = 1;
+            int t = 100;
+            string Answer = Console.ReadLine();
+            if (Answer == y)
+
+                for (i = 1; i < a;)
+                {
+                    Console.WriteLine("Ваше число больше {0}?", i);
+                    if (Console.ReadLine() == y)
+                    {
+                        i++;
+                    }
+
+                    else Console.WriteLine("Ваше число равно {0}", i);
+
+                }
+
+            else if (Answer == n) Console.WriteLine("Ваше число больше 50?");
+            if (Console.ReadLine() == y)
+                while (a < t)
+                {
+                    Console.WriteLine("Ваше число меньше {0}?", t);
+                    if (Console.ReadLine() == y)
+                    {
+                        t--;
+                    }
+                    else Console.WriteLine("Ваше число равно {0}", t);
+
+                }
+            else Console.WriteLine("Ваше число 50");
+
+            Console.ReadKey();
         }
 
 
